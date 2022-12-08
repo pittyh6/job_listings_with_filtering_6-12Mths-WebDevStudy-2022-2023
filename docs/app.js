b3899f6ac7a928d706a1010e59cc9dd785e8bf1c
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-12-08 14:15:42 +1100

    gettind date from json and add to elements

--- docs/app.js
+++ docs/app.js
@@ -3,12 +3,28 @@ import data from './data.json' assert {type: 'json'};
 // Elements Document
 const logoEl = $(".logo-img")
 const companyNameEl = $(".company-name")
+const newEl = $(".company-tag-new")
+const featuredEl = $(".company-tag-featured")
+const positionEl = $(".job-title")
+const jobPostedEl = $(".job-day-posted")
+const jobType = $(".job-type")
+const jobLocation = $(".job-location")
+
+
 
 logoEl.attr("src", data[1].logo)
 companyNameEl.text(data[1].company)
+if (data[1].new !== true) { // add elements new and featured  ---
+    newEl.hide()
+} else {
+    newEl.show()
+}
+if (data[1].featured !== true) {
+    featuredEl.hide()
+} else {
+    featuredEl.show()
+} // ------
+positionEl.text(data[1].position)
 
-console.log(companyNameEl)
-
-console.log(data[1].company)
 
 
